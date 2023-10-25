f173b0835da8808fba26e40aa0e527b00f43503e
kevokl <lacartax@gmail.com>
2023-10-24 20:36:03 -0600

    intento 9, ahora es personal

--- botinesFutbol/settings.py
+++ botinesFutbol/settings.py
@@ -135,21 +135,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "aStatic/"
+STATIC_URL = "static/"
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'aStatic'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'aStatic')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / "aStatic"
+    BASE_DIR / "static"
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = '/Amedia/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Amedia')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
