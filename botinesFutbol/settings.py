f31d1b4e10801161e8793feea76c097b6a02fe74
kevokl <lacartax@gmail.com>
2023-10-24 20:31:34 -0600

    intento 8, ahora mas intento que nunca

--- botinesFutbol/settings.py
+++ botinesFutbol/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 
 STATIC_URL = "aStatic/"
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'aStatic'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'aStatic')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
